19b06ce326011d364642375168f13789ea63bdf0
MiguelRequenaR <m.angelrequena20@gmail.com>
2026-06-10 19:10:42 -0500

    feat: Sección de about agregada.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      }
+    }
   }
 });
