f86384ed2719d86356d346979c0b38fc8751f2f2
MiguelRequenaR <m.angelrequena20@gmail.com>
2026-06-14 19:38:29 -0500

    fix: Solución en errores de archivos de lenis y gsap.

--- src/scripts/lenis.js
+++ src/scripts/lenis.js
@@ -1,14 +1,30 @@
 import Lenis from 'lenis';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-const lenis = new Lenis({
-  smoothWheel: true,
-  duration: 1.5,
-  easing: (t) => Math.min(1, 1.001 - 2 ** (-10 * t)),
-});
+let lenis;
+let rafId;
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+function initLenis() {
+  if (lenis) {
+    lenis.destroy();
+    cancelAnimationFrame(rafId);
+  }
+
+  lenis = new Lenis({
+    smoothWheel: true,
+    duration: 1.5,
+    easing: (t) => Math.min(1, 1.001 - 2 ** (-10 * t)),
+  });
+
+  lenis.on('scroll', ScrollTrigger.update);
+
+  function raf(time) {
+    lenis.raf(time);
+    rafId = requestAnimationFrame(raf);
+  }
+
+  rafId = requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf);
+document.addEventListener("astro:page-load", initLenis);
+initLenis();
